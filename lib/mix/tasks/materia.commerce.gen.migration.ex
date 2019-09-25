3dee56d25bd9fdbb1e9983add6c666368c08c5a4
mike.kodama <mike.kodama@karabiner.tech>
2019-09-25 12:25:02 +0900

    feat: migration_task_refactor

--- lib/mix/tasks/materia.commerce.gen.migration.ex
+++ lib/mix/tasks/materia.commerce.gen.migration.ex
@@ -1,11 +1,7 @@
 defmodule Mix.Tasks.MateriaCommerce.Gen.Migration do
   @shortdoc "Generates MateriaCommerce's migration files."
 
-  use Mix.Task
-
-  import Mix.Generator
-  import Mix.Tasks.Guardian.Db.Gen.Migration
-  alias Mix.Tasks.Materia.Gen.Migration, as: MateriaMigration
+  alias Mix.Tasks.Materia.Gen.Migration
 
   @migrations_file_path "priv/repo/migrations"
   @migration_module_path "deps/materia_commerce/lib/mix/templates"
@@ -13,7 +9,7 @@ defmodule Mix.Tasks.MateriaCommerce.Gen.Migration do
   @doc false
   def run(args) do
     args
-    |> MateriaMigration.setting_migration_module_path(@migration_module_path)
-    |> MateriaMigration.create_migration_files(@migrations_file_path, "materia_commerce")
+    |> Migration.setting_migration_module_path(@migration_module_path)
+    |> Migration.create_migration_files(@migrations_file_path, "materia_commerce")
   end
 end
