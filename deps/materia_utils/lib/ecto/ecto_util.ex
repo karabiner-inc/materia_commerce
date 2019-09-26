23854547aedd8f12ecf12f29d2a4453d7dae0d87
kuboyaaan <hirotaka.kuboyama@gmail.com>
2019-09-26 17:51:14 +0900

    コンフリクトのため再push

---
+++ deps/materia_utils/lib/ecto/ecto_util.ex
@@ -0,0 +1,450 @@
+defmodule MateriaUtils.Ecto.EctoUtil do
+  @moduledoc """
+  Ecto関連の操作機能
+
+
+  ヒストリカルテーブルに対して、現在断面や未来の履歴を返す共通実装を含む
+
+  ヒストリカルテーブルは必ずレコードの有効時刻を表す
+  start_datetime、end_datetimeを持つテーブルであること。
+  また、同一データ同士でstard_datetimeとend_datetimeの期間重複がないことが前提となる。
+
+  """
+  import Ecto.Query, warn: false
+
+  @doc """
+  SQL直接実行
+
+  EctoのSQLビルダを使用せずにSQLを直接実行する
+  Ecto.Adapters.SQL.query()のWrapper関数
+  戻り値を[%{row1},%{row2}...]形式で返す。
+
+  ## Examples
+  ```
+  # iex> EctoUtil.query(MyApp.Repo, "select * from users", params)
+  # [${id: => 1, name: => "userA name" },${id: => 2, name: => "userB name" }]
+  ```
+
+  """
+  @spec query(Repo, string, [list]) :: [list]
+  def query(repo, sql, params) do
+    Ecto.Adapters.SQL.query(repo, sql, params)
+    |> result_to_map_list()
+  end
+
+  @doc """
+  汎用Selectエンドポイント用
+  汎用検索向けなのでロックは取らない
+
+  ## パラメータ
+
+    - params: where句で使用したいパラメータ
+
+  ## Examples
+
+  　  POSTで以下のparamsが指定される想定
+    and もしくは orはマップのリストで指定する
+    and もしくは orが必要ない場合は指定しなくてよい
+
+    ```
+    params = {
+        "and": [{"stock_id": 1}, {"purchase_detail_id": 3}],
+        "or": [{"status": 1}, {"status": 3}]
+    }
+    ```
+
+    上記の例で実行されるクエリのWHERE条件
+    ```
+     WHERE ((s0.`status` = ?) OR (s0.`status` = ?)) AND ((s0.`stock_id` = ?) AND (s0.`purchase_detail_id` = ?))
+    ```
+
+  """
+  @spec select_by_param(Repo, Ecto.Schema, [list]) :: [list]
+  def select_by_param(repo, schema, params) do
+  
+    query = from(t in schema)
+    query = build_query_by_params(query, params)
+    repo.all(query)
+
+  end
+
+  @spec delete_by_param(Repo, Ecto.Schema, Atom, DateTime, [list]) :: [list]
+  def delete_by_param(repo, schema, base_datetime_column, base_datetime, params) do
+
+    query = from(t in schema)
+    query = query
+      |> where([t], field(t, ^base_datetime_column) <= ^base_datetime)
+    query = build_query_by_params(query, params)
+    repo.delete_all(query)
+
+  end
+
+  defp build_query_by_params(query, params) do
+    # 汎用検索向けなのでロックは取らない
+    query =
+      cond do
+        params == nil ->
+          query
+
+        params["or"] != nil ->
+          params["or"]
+          |> Enum.reduce(query, fn elem, query ->
+            key = hd(Map.keys(elem))
+            value = Map.get(elem, key)
+            key = String.to_atom(key)
+            or_keyword = [{key, value}]
+            or_where(query, ^or_keyword)
+          end)
+
+        true ->
+          query
+      end
+
+    query =
+      cond do
+        params == nil ->
+          query
+
+        params["paging"] != nil ->
+          page_value = params["paging"]["page"]
+          limit_value = params["paging"]["limit"]
+          offset_value = offset_calculation(page_value, limit_value)
+
+          query
+          |> limit(^limit_value)
+          |> offset(^offset_value)
+
+        true ->
+          query
+      end
+
+    query =
+      if params != nil do
+        params
+        |> Map.keys()
+        |> Enum.reject(fn operator -> operator == "or" or operator == "paging" end)
+        |> Enum.reduce(query, fn operator, query ->
+          params[operator]
+          |> Enum.reduce(query, fn elem, query ->
+            key = hd(Map.keys(elem))
+            value = Map.get(elem, key)
+            key = String.to_atom(key)
+
+            case operator do
+              "and" ->
+                query
+                |> where([s], field(s, ^key) == ^value)
+
+              "not" ->
+                query
+                |> where([s], field(s, ^key) != ^value)
+
+              "in" ->
+                query
+                |> where([s], field(s, ^key) in ^value)
+
+              "greater" ->
+                query
+                |> where([s], field(s, ^key) > ^value)
+
+              "greater_equal" ->
+                query
+                |> where([s], field(s, ^key) >= ^value)
+
+              "less" ->
+                query
+                |> where([s], field(s, ^key) < ^value)
+
+              "less_equal" ->
+                query
+                |> where([s], field(s, ^key) <= ^value)
+
+              "order_by" ->
+                case value do
+                  "asc" ->
+                    query
+                    |> order_by(asc: ^key)
+
+                  "desc" ->
+                    query
+                    |> order_by(desc: ^key)
+
+                  _ ->
+                    ""
+                end
+
+              "like" ->
+                query
+                |> where([s], like(field(s, ^key), ^"%#{value}%"))
+
+              "forward_like" ->
+                query
+                |> where([s], like(field(s, ^key), ^"#{value}%"))
+
+              "backward_like" ->
+                query
+                |> where([s], like(field(s, ^key), ^"%#{value}"))
+
+              _ ->
+                query
+            end
+          end)
+        end)
+      else
+        query
+      end
+  end
+
+  def offset_calculation(page_condition, limit_condition) do
+    offset_value = (page_condition - 1) * limit_condition
+  end
+
+  defp result_to_map_list(nil) do
+    #戻りが無いSQLの場合、nilで処理する
+    nil
+  end
+  defp result_to_map_list({:error, error}) do
+    #エラーはスルー
+    {:error, error}
+  end
+
+  defp result_to_map_list({:ok, result}) do
+
+    columns = result.columns
+    case columns do
+      nil ->
+        [num_rows: result.num_rows]
+      _ ->
+        rows = result.rows
+        list_maps = Enum.map(rows, fn row -> row_columns_to_map(row, columns) end)
+    end
+
+  end
+
+  defp row_columns_to_map(row, columns) do
+    map_result =
+      Enum.map(Enum.with_index(row, 0), fn {k, i} -> [Enum.at(columns, i), k] end)
+      |> Enum.map(fn [a, b] -> {String.to_atom(a), convert(b)} end)
+      |> Map.new()
+  end
+
+  def convert({{year, month, day}, {hour, minites, sec, msec}}) do
+    #日時解釈できるものはDateTimeに変換
+    Timex.to_datetime({{year, month, day}, {hour, minites, sec, msec}})
+  end
+
+  def convert(attr) do
+    attr
+  end
+
+  @doc """
+  ヒストリカルテーブル用のSelectエンドポイント用
+
+  対処となるRepo,スキーマ及び
+  基準となる時刻と検索キーをキーワドリスト[{k, v}]形式で指定すると
+  履歴が挿入されたヒストリカルテーブルから基準時刻時点の断面データを返す
+
+
+  ###Example
+
+  iex(1)> MateriaUtils.Ecto.EctoUtil.list_current_history(MateriaCommerce.Test.Repo, MateriaCommerce.Products.Item, Timex.now(), [{:item_code, "ICZ1000"}])
+
+
+  """
+  @spec list_current_history(Repo, Ecto.Schema, DateTime, [Keyword]) :: List
+  def list_current_history(repo, schema, base_datetime, keyword_list) do
+    # 期間の検索条件付与
+    list_current_history(repo, schema, base_datetime, keyword_list, nil)
+
+  end
+
+  @doc """
+  ヒストリカルテーブル用のSelectエンドポイント用の汎用検索
+
+  対象となるRepo, Schema, 基準日時, 一意キー, 検索キーを指定すると､
+  基準事項時点の断面データをベースに汎用検索結果を返す｡
+
+  """
+  def list_current_history(repo, schema, base_datetime, primary_keywords, condition_keywords) do
+    query_current_history(repo, schema, base_datetime, primary_keywords, condition_keywords)
+    |> lock("FOR UPDATE")
+    |> repo.all()
+  end
+
+  @doc """
+  ヒストリカルテーブル用のSelectエンドポイント用の汎用検索
+
+  対象となるRepo, Schema, 基準日時, 一意キー, 検索キーを指定すると､
+  基準事項時点の断面データをベースに汎用検索結果を返し､ロックは取得しない
+
+  """
+  def list_current_history_no_lock(repo, schema, base_datetime, primary_keywords, condition_keywords) do
+    query_current_history(repo, schema, base_datetime, primary_keywords, condition_keywords)
+    |> repo.all()
+  end
+
+  @doc """
+  ヒストリカルテーブル用のSelectエンドポイント用の汎用検索のQueryを返す
+  """
+  def query_current_history(repo, schema, base_datetime, primary_keywords, condition_keywords) do
+    schema
+      |> build_query_by_params(condition_keywords)
+      |> where([s], s.start_datetime <= ^base_datetime and s.end_datetime >= ^base_datetime)
+      |> add_pk(primary_keywords)
+  end
+
+  @doc """
+  ヒストリカルテーブル用のSelectエンドポイント用
+
+  対処となるRepo,スキーマ及び
+  基準となる時刻と検索キーをキーワドリスト[{k, v}]形式で指定すると
+  履歴が挿入されたヒストリカルテーブルから基準時刻から未来の予定として登録された履歴の一覧を返す
+  履歴更新時に先日付のデータをクリアする用途を想定する為、
+  statt_datetime == base_datetime
+  のデータを含む
+
+  ###Example
+
+  iex(1)> MateriaUtils.Ecto.EctoUtil.list_future_histories(MateriaCommerce.Test.Repo, MateriaCommerce.Products.Item, Timex.now(), [{:item_code, "ICZ1000"}])
+
+
+  """
+  @spec list_future_histories(Repo, Ecto.Schema, DateTime, [Keyword]) :: List
+  def list_future_histories(repo, schema, base_datetime, keyword_list) do
+    sql = schema
+      |> where([s], s.start_datetime >= ^base_datetime)
+      |> add_pk(keyword_list)
+      |> lock("FOR UPDATE")
+      |> repo.all()
+  end
+
+  @doc """
+  ヒストリカルテーブル用の更新エンドポイント用
+
+  対処となるRepo,スキーマ及び
+  基準となる時刻と検索キーをキーワドリスト[{k, v}]形式で指定すると
+  履歴が挿入されたヒストリカルテーブルから基準時刻から未来の予定として登録された履歴を削除する。
+  データの更新用途を想定する為、
+  statt_datetime == base_datetime
+  のデータを含む
+
+  ###Example
+
+  iex(1)> base_datetime = MateriaUtils.Calendar.CalendarUtil.parse_iso_extended_z("2018-12-17 09:00:00Z")
+  iex(2)> MateriaUtils.Ecto.EctoUtil.delete_future_histories(MateriaCommerce.Test.Repo, MateriaCommerce.Products.Item, base_datetime, [{:item_code, "ICZ1000"}])
+
+  """
+  @spec delete_future_histories(Repo, Ecto.Schema, DateTime, [Keyword]) :: {integer(), nil | [term()]}
+  def delete_future_histories(repo, schema, base_datetime, keyword_list) do
+    sql = schema
+      |> where([s], s.start_datetime >= ^base_datetime)
+      |> add_pk(keyword_list)
+      |> repo.delete_all()
+  end
+
+  @doc """
+  ヒストリカルテーブル用のSelectエンドポイント用
+
+  対処となるRepo,スキーマ及び
+  基準となる時刻と検索キーをキーワドリスト[{k, v}]形式で指定すると
+  履歴が挿入されたヒストリカルテーブルから基準時刻より過去の履歴として登録された履歴の一覧を返す
+  statt_datetime == base_datetime
+  のデータを含まない
+
+  ###Example
+
+  iex(1)> MateriaUtils.Ecto.EctoUtil.list_past_histories(MateriaCommerce.Test.Repo, MateriaCommerce.Products.Item, Timex.now(), [{:item_code, "ICZ1000"}])
+
+
+  """
+  @spec list_past_histories(Repo, Ecto.Schema, DateTime, [Keyword]) :: List
+  def list_past_histories(repo, schema, base_datetime, keyword_list) do
+    sql = schema
+      |> where([s], s.start_datetime < ^base_datetime)
+      |> add_pk(keyword_list)
+      |> lock("FOR UPDATE")
+      |> repo.all()
+  end
+
+  @doc """
+  query1 = schema |> select([s], [s.item_code, max(s.stard_datetime)]) |> from([s]) |> where([s], s.start_datetime < ^base_datetime) |> where(^[{:item_code, "ICZ1000"}])
+  query2 = from(s in MateriaCommerce.Products.Item, join: s2 in subquery(query1))
+  query2 = schema |> where(^[{:item_code, "ICZ1000"}])
+  """
+  #@spec list_recent_history(Repo, Ecto.Schema, DateTime, [Keyword]) :: List
+  #def list_recent_history(repo, schema, base_datetime, keyword_list) do
+  #  sql = schema
+  #  |> where([s], s.start_datetime > ^base_datetime)
+  #  |> add_pk(keyword_list)
+  #  |> lock("FOR UPDATE")
+  #  |> repo.all()
+  #end
+
+  @doc """
+  MateriaUtils.Ecto.EctoUtil.list_recent_history(MateriaCommerce.Test.Repo, MateriaCommerce.Products.Item, Timex.now(), [{:item_code, "ICZ1000"}])
+  """
+  @spec list_recent_history(Repo, Ecto.Schema, DateTime, [Keyword]) :: List
+  def list_recent_history(repo, schema, base_datetime, keyword_list) do
+    table_name = Ecto.get_meta(struct(schema), :source)
+    select_string = keyword_list
+      |> Enum.reduce(
+        "",
+        fn (keyword, acc) ->
+          {k, v} = keyword
+          if acc != "", do: "#{acc}, #{k}", else: "#{acc}#{k}"
+        end
+      )
+    and_string = keyword_list
+      |> Enum.reduce(
+        "",
+        fn (keyword, acc) ->
+          {k, v} = keyword
+          if acc != "", do: "#{acc} and #{k} = '#{v}'", else: "#{acc}#{k} = '#{v}'"
+        end
+      )
+    join_on_string = keyword_list
+      |> Enum.reduce(
+        "",
+        fn (keyword, acc) ->
+          {k, v} = keyword
+          "#{acc} and s1.#{k} = s2.#{k}"
+        end
+      )
+    sql = "
+      select
+        *
+      from
+        (
+          select
+            *
+          from
+            #{table_name}
+          where
+            #{and_string}
+        ) s1
+        join(
+          select
+            #{select_string} ,max( start_datetime ) as start_datetime
+          from
+            #{table_name}
+          where
+            start_datetime < $1 and
+            #{and_string}
+          group by
+            #{select_string}
+        ) s2 on
+        s1.start_datetime = s2.start_datetime
+        #{join_on_string}"
+
+    query(repo, sql, [base_datetime])
+  end
+
+  def add_pk(sql, key_word_list) do
+    # 主キーの検索条件付与
+    sql = [key_word_list]
+      |> Enum.reduce(sql, fn(key_word, acc) ->
+        acc
+        |> where(^key_word)
+      end)
+  end
+end
